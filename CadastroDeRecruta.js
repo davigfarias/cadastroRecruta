6de1289dd9352a1722ef66677487dde5d76b27ff
Dave Farias <166950782+davigfarias@users.noreply.github.com>
2024-11-16 15:01:56 -0300

    Create CadastroDeRecruta.js

---
+++ CadastroDeRecruta.js
@@ -0,0 +1,11 @@
+let firstname
+let surname
+let studyField
+let birthdate
+
+firstname = prompt('Como você se chama? (Só o primeiro nome')
+surname = prompt('Qual seu sobrenome?')
+studyField = prompt('Qual seu campo de estudo?')
+birthdate = prompt('Quando você nasceu?')
+
+console.log("Olá " + firstname + " " + surname + ", prazer em conhecer. Sua data de nascimento é " + birthdate + " e você estuda " + studyField)
